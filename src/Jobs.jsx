3c8f82aadb130d1ec0c3e0ab32c1c15757192f1a
code1511 <anch20001511@gmail.com>
2023-04-09 13:11:37 +0530

    added job page

---
+++ src/Jobs.jsx
@@ -0,0 +1,44 @@
+// rel="noopener noreferrer"
+
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Jobs = () => {
+
+    return (
+        <Container>
+            <h1>Jobs in Hyderabad</h1>
+            <br />
+            <hr></hr>
+            <Row>
+                <Col>
+                    <a href="https://qualcomm.wd5.myworkdayjobs.com/External?locationCountry=c4f78be1a8f14da0ab49ce1162348a5e&locations=f965745320d20101f24479a65d0a0000&workerSubType=c6ccad57afa3010d92328a96f4007b06" rel="noopener noreferrer" target={"_blank"}>
+                        <h6>Qualcome careers</h6>
+                    </a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://www.uber.com/in/en/careers/list/?location=IND-Telangana-Hyderabad"> <h6>Uber careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://careers.servicenow.com/careers/jobs?location=Hyderabad,%20Telangana,%20India&woe=7&stretchUnit=MILES&stretch=10&page=1"> <h6> Servicenow careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://jpmc.fa.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1001/requisitions?location=Hyderabad%2C+Telangana%2C+India&locationId=300000081155702&locationLevel=city&mode=location&radius=25&radiusUnit=MI"> <h6>JPMC careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://www.deshawindia.com/careers"> <h6>DE SAW careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://www.amazon.jobs/en/search?offset=0&result_limit=10&sort=relevant&job_type%5B%5D=Full-Time&country%5B%5D=IND&city%5B%5D=Hyderabad&distanceType=Mi&radius=24km&is_manager%5B%5D=0&latitude=17.39491&longitude=78.47073&loc_group_id=&loc_query=Hyderabad%2C%20TS%2C%20India&base_query=&city=Hyderabad&country=IND&region=Telangana&county=Hyderabad&query_options=&"> <h6>Amazon careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://careers.microsoft.com/professionals/us/en/search-results?rk=l-l-hyderabad"> <h6>Microsoft careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://careers.google.com/jobs/results/?category=DATA_CENTER_OPERATIONS&category=DEVELOPER_RELATIONS&category=HARDWARE_ENGINEERING&category=MANUFACTURING_SUPPLY_CHAIN&category=NETWORK_ENGINEERING&category=PRODUCT_MANAGEMENT&category=PROGRAM_MANAGEMENT&category=SOFTWARE_ENGINEERING&category=TECHNICAL_INFRASTRUCTURE_ENGINEERING&category=TECHNICAL_SOLUTIONS&category=TECHNICAL_WRITING&hl=en&jlo=en-US&location=Hyderabad,%20Telangana,%20India"> <h6>Google careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://careers.oracle.com/#en/sites/jobsearch/requisitions?location=HYDERABAD%2C+TELANGANA%2C+India&locationId=300001842985230&locationLevel=city&mode=location&radius=0&radiusUnit=MI"> <h6>Oracle careers</h6></a>
+
+                </Col>
+                <Col>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://careers.arcesium.com/search/?createNewAlert=false&q=&locationsearch=hyderabad&optionsFacetsDD_country=&optionsFacetsDD_dept="> <h6>Arcesium Careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://paypal.eightfold.ai/careers?location=Hyderabad%2C%20Telangana%2C%20India&pid=274893472971&domain=paypal.com&sort_by=relevance"> <h6>PayPal Careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://www.goldmansachs.com/careers/apply/index.html"><h6> Goldman sachs Careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://jobs.apple.com/en-in/search?search=Software%20Engineer&sort=relevance&location=hyderabad-HY1"> <h6>Apple Careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://salesforce.wd1.myworkdayjobs.com/External_Career_Site?d=cta-ap-sjb-1&locations=5bb04c58f6904966a9202276f06f6766&timeType=109431f5de7f4faaaea2e4b765ce1dfa"><h6>Salesforce careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://jobs.carelonglobal.com/in/jobs/?search=&location=Hyderabad&pagesize=20#results"> <h6>Legato Careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://www.notion.so/careers"> <h6> Notion Careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://careers.cloud.com/jobs/search?page=1&country_codes%5B%5D=IN&cities%5B%5D=Hyderabad&query="> <h6>Citrix Careers</h6></a>
+                    <a rel="noopener noreferrer" target={"_blank"} href="https://careers.teradata.com/jobs?page=1&City=Hyderabad%2C%20TG&remote=false&keyword=software%20engineer"> <h6> TeraData Careers</h6></a>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Jobs;
